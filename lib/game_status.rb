9211698e1e9dbbabc2fd0cee21c7c90ed7190792
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-19 18:37:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,11 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  thw = [0,1,2],
+  mhw = [3,4,5],
+  bhw = [6, 7,8],
+  lvw = [0,3,6],
+  mvw = [1,4,7],
+  rvw = [2,5,8],
+]
